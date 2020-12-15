12ff647caed4724dff58508acb63067fccc02755
ttoortik <ttoortik@mail.ru>
2020-12-15 14:09:44 +0400

    Initial commit

---
+++ Assets/Photon Unity Networking/PreMade/RPSLS_MP/Scripts/Leave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leave : MonoBehaviour
+{
+    public GameObject back;
+    public GameObject main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            main.gameObject.SetActive(false);
+            back.gameObject.SetActive(true);
+        }
+    }
+}
